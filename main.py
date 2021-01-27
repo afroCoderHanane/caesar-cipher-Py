f7104cb30a99ce63e7c0b5007c3879aced0f0c5b
Abdoul Hanane <gbadhanane@gmail.com>
2021-01-27 22:20:12 +0000

    personalized caesar cypher: Don't encrypt space and numbers

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
+from art import logo
+print (logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def caesar_cypher(text, shift, dir_):
   cypher_text=""
-  for i in range(len(text)):
-    position= alphabet.index(text[i])
-    if(dir_ == "encode"):
-      new_position = position+shift
-      if new_position>25:
-        new_position = new_position%26
-      cypher_text += alphabet[new_position]
+  for char in text:
+    if char in alphabet:
+      position= alphabet.index(char)
+      if(dir_ == "encode"):
+        new_position = position+shift
+        if new_position>25:
+          new_position = new_position%26
+        cypher_text += alphabet[new_position]
+      else:
+        new_position = position - shift
+        if(new_position<0):
+          new_position+=26
+        cypher_text+=alphabet[new_position]
     else:
-      new_position = position - shift
-      if(new_position<0):
-         new_position+=26
-      cypher_text+=alphabet[new_position]
+      cypher_text+=char
 
   print(f"The encoded text is {cypher_text}")
-   
-caesar_cypher(text, shift, dir_=direction)
+#logo
+
+#
+choice = "no"
+while(choice !="no"):
+  caesar_cypher(text, shift, dir_=direction)
+  choice = input("Type 'yes' to enter again or 'no' to end the program\n").lower()
+
+
 # def decrypt(cyphered_text, shift):
 #     decyphered_text=""
 #     for i in range (len(cyphered_text)):
